12e7feae5cbd3538eaba1ffcf40ae9dff676d79e
Kevin Ramos <kevin.ramos3@hotmail.com>
2023-02-24 11:44:21 +0000

    This code should let the user to print a string using printf as a function

---
+++ 0x00-Hello_world/5-printfString.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - will print a string of characters using printf
+*Return: should return 0 if success
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
