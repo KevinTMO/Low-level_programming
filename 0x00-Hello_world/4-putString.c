20411c6a66447f56374d1fda66005dba692bfe1e
Kevin Ramos <kevin.ramos3@hotmail.com>
2023-02-24 11:43:43 +0000

    This code should enable the user to print a string

---
+++ 0x00-Hello_world/4-putString.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - will print a string of characters
+*Return: should return 0 if success
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
