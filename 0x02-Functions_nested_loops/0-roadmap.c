75cd4b96fbf4f8586aaa40befd85d449bb09cf8d
Kevin Ramos <kevin.ramos3@hotmail.com>
2023-03-01 11:01:37 +0000

    Creating a program to print Roadmap using putchar

---
+++ 0x02-Functions_nested_loops/0-roadmap.c
@@ -0,0 +1,19 @@
+#include "roadmap.h"
+
+/**
+*main - will print the string roadmap using _putchar
+*Return: will return 0 if success
+*/
+
+int main(void)
+{
+	_putchar('R');
+	_putchar('o');
+	_putchar('a');
+	_putchar('d');
+	_putchar('m');
+	_putchar('a');
+	_putchar('p');
+	_putchar('\n');
+	/*return (0);*/
+}
