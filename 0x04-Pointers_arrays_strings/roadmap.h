865ac155f234fb2b43a415d4ff2b53556f36bb50
Kevin Ramos <kevin.ramos3@hotmail.com>
2023-03-09 17:44:03 +0000

    Updating task 4 prototype in the header

---
+++ 0x04-Pointers_arrays_strings/roadmap.h
@@ -0,0 +1,36 @@
+#ifndef ROADMAP_H
+#define ROADMAP_H
+
+/* STD LIBRARIES */
+
+#include <stdio.h>
+
+/* _putchar will print a char passed to stdout */
+
+int _putchar(int c);
+
+/* resetTo98 will reset the value of the parameter passed using a pointer */
+
+void resetTo98(int *n);
+
+/* swapInts will swap the values of two var integers */
+
+void swapInts(int *a, int *b);
+
+/* strLen will return the len of a string */
+
+int strLen(char *str);
+
+/* _puts will print a string passed followed by a newline */
+
+void _puts(char *str);
+
+/* printRevStr will print a string in reverse */
+
+void printRevStr(char *str);
+
+/* revStr will reverse a string */
+
+void revStr(char *str);
+
+#endif
